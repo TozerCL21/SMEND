d8079c78b93ce82bf241107de4b5c7d3a36bb7a0
TozerCL21 <connor.tozer2000@gmail.com>
2016-12-16 09:59:19 -0500

    Create authlib exec data
    Will eventually run on Wine. (In theory)

--- authlib.sh
+++ authlib.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+info=$(id -u)
+case $id in
+1000) echo "FALSE" >> /usr/bin/smend/data/auth.lib/1.txt; sleep 2; exit;;
+0) echo "TRUE" >> /usr/bin/smend/data/auth.lib/1.txt; sleep 2; exit;;
+esac
 
